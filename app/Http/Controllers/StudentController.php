208353c474d85ac5c68b5006ae4acfc8f9f84a6a
olamide <olamzey@gmail.com>
2023-08-29 15:02:45 +0100

    student profile

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -30,6 +30,18 @@ class StudentController extends Controller
     }
 
     public function insert(Request $request){
+
+        request()->validate([
+            'email' => 'required|email|unique:users',
+            'height' => 'max:10',
+            'weight' => 'max:10',
+            'blood_group' => 'max:10',
+            'mobile_number' => 'max:15 | min:11',
+            'religion' => 'max:50',
+            'admission_number' => 'max:50',
+            'roll_number' => 'max:50',
+        ]);
+
         $student = new User();
         $student->name = trim($request->name);
         $student->admission_number = trim($request->admission_number);
@@ -72,6 +84,81 @@ class StudentController extends Controller
         $student->save();
 
         return redirect('admin/student/list')->with('success', 'Student Successfully Created');
+
+    }
+
+    public function edit($id){
+
+        $data['getRecord'] = User::getSingle($id);
+        if (!empty($data['getRecord'])) {
+            
+            $data['header_title'] = 'Edit Student';
+            $data['getClass'] = ClassModel::getClass();
+            return view('admin/student/edit', $data);
+        }else{
+            abort(404);
+        }
+
+    }
+
+    public function update($id, Request $request){
+        
+        request()->validate([
+            'email' => 'required|email|unique:users,email,'.$id,
+            'height' => 'max:10',
+            'weight' => 'max:10',
+            'blood_group' => 'max:10',
+            'mobile_number' => 'max:15 | min:11',
+            'religion' => 'max:50',
+            'admission_number' => 'max:50',
+            'roll_number' => 'max:50',
+        ]);
+
+        $student = User::getSingle($id);
+        $student->name = trim($request->name);
+        $student->admission_number = trim($request->admission_number);
+        $student->roll_number = trim($request->roll_number);
+        $student->class_id = trim($request->class_id);
+
+        if (!empty($request->date_of_birth)) {
+            $student->date_of_birth = trim($request->date_of_birth);
+        }
+
+        if (!empty($request->file('profile_pic'))) {
+            
+            $ext = $request->file('profile_pic')->getClientOriginalExtension();
+            $file = $request->file('profile_pic');
+            $randomStr = date('Ymdhis').Str::random(20);
+            $filename = strtolower($randomStr ).'.'.$ext;
+            $file->move('upload/profile/', $filename);
+
+            $student->profile_pic = $filename;
+             
+        }
+        $student->religion = trim($request->religion);
+        $student->mobile_number = trim($request->mobile_number);
+
+        if (!empty($request->admission_date)) {
+            
+            $student->admission_date = trim($request->admission_date);
+        }
+        
+        $student->blood_group = trim($request->blood_group);
+        $student->height = trim($request->height);
+        $student->weight = trim($request->weight);
+        $student->status = trim($request->status);
+        $student->gender = trim($request->gender);
+        $student->address = trim($request->address);
+        $student->email = trim($request->email);
+        
+        if (!empty($request->password)) {
+
+            $student->password = hash::make($request->password);
+        }
+
+        $student->save();
+
+        return redirect('admin/student/list')->with('success', 'Student Successfully Updated');
 
     }
     
--- resources/views/admin/student/edit.blade.php
+++ resources/views/admin/student/edit.blade.php
@@ -9,7 +9,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Add New Student</h1>
+            <h1>Edit Student</h1>
           </div>
           
         </div>
@@ -24,26 +24,26 @@
           <div class="col-md-12">
             <!-- general form elements -->
             <div class="card card-primary">
-             
+            
               <form method="POST" enctype="multipart/form-data">
                 {{ csrf_field() }}
                 <div class="card-body">
                     <div class="row">
                         <div class="form-group col-md-12">
                             <label >Full Name <span style="color: red">*</span></label>
-                            <input type="text" class="form-control" name="name" value="{{ old('name' )}}" required placeholder="Full Name">
+                            <input type="text" class="form-control" name="name" value="{{ old('name',$getRecord->name )}}" required placeholder="Full Name">
                             <div style="color: red">{{$errors->first('name')}}</div>
                           </div>
 
                         <div class="form-group col-md-6">
                             <label >Admission Number <span style="color: red">*</span></label>
-                            <input type="text" class="form-control" name="admission_number" value="{{ old('admission_number' )}}" required placeholder="Admission Number">
+                            <input type="text" class="form-control" name="admission_number" value="{{ old('admission_number', $getRecord->admission_number )}}" required placeholder="Admission Number">
                             <div style="color: red">{{$errors->first('admission_number')}}</div>
                         </div>
 
                         <div class="form-group col-md-6">
                             <label >Roll Number <span style="color: red"></span></label>
-                            <input type="text" class="form-control" name="roll_number" value="{{ old('roll_number' )}}" placeholder="Roll Number">
+                            <input type="text" class="form-control" name="roll_number" value="{{ old('roll_number', $getRecord->roll_number )}}" placeholder="Roll Number">
                             <div style="color: red">{{$errors->first('roll_number')}}</div>
 
                         </div>
@@ -53,7 +53,7 @@
                             <select name="class_id" id="" class="form-control" required>
                                 <option value="">Select Class</option>
                                 @foreach ($getClass as $class)
-                                <option {{(old('class_id') == $class->id) ? 'selected' : ''}} value="{{$class->id}}">{{$class->name}}</option>
+                                <option {{(old('class_id', $getRecord->class_id) == $class->id) ? 'selected' : ''}} value="{{$class->id}}">{{$class->name}}</option>
                                 @endforeach
                             </select>
                             <div style="color: red">{{$errors->first('class_id')}}</div>
@@ -64,8 +64,8 @@
                             <label>Gender <span style="color: red">*</span></label>
                             <select name="gender" id="" class="form-control" required>
                                 <option  value="">Select Gender</option>
-                                <option {{(old('gender') == 'Male') ? 'selected' : ''}} value="male">Male</option>
-                                <option {{(old('gender') == 'Female') ? 'selected' : ''}} value="male">Female</option>
+                                <option {{(old('gender', $getRecord->gender) == 'male') ? 'selected' : ''}} value="male">Male</option>
+                                <option {{(old('gender', $getRecord->gender) == 'female') ? 'selected' : ''}} value="female">Female</option>
                                 
                             </select>
                             <div style="color: red">{{$errors->first('gender')}}</div>
@@ -73,7 +73,7 @@
 
                         <div class="form-group col-md-6">
                             <label >Date of Birth <span style="color: red">*</span></label>
-                            <input type="date" class="form-control" name="date_of_birth" value="{{ old('date_of_birth' )}}" required>
+                            <input type="date" class="form-control" name="date_of_birth" value="{{ old('date_of_birth', $getRecord->date_of_birth )}}" required>
                             
                             <div style="color: red">{{$errors->first('date_of_birth')}}</div>
 
@@ -81,25 +81,25 @@
 
                         <div class="form-group col-md-6">
                             <label >Religion <span style="color: red"></span></label>
-                            <input type="text" class="form-control" name="religion" value="{{ old('religion' )}}" placeholder="Religion">
+                            <input type="text" class="form-control" name="religion" value="{{ old('religion', $getRecord->religion)}}" placeholder="Religion">
                             <div style="color: red">{{$errors->first('religion')}}</div>
                         </div>
 
                         <div class="form-group col-md-6">
                             <label > Address <span style="color: red">*</span></label>
-                            <input type="text" class="form-control" name="address" value="{{ old('address' )}}" placeholder="Address" required>
+                            <input type="text" class="form-control" name="address" value="{{ old('address', $getRecord->address )}}" placeholder="Address" required>
                             <div style="color: red">{{$errors->first('address')}}</div>
                          </div>
 
                         <div class="form-group col-md-6">
                             <label >Phone Number <span style="color: red"></span></label>
-                            <input type="text" class="form-control" name="mobile_number" value="{{ old('mobile_number' )}}" placeholder="Phone Number">
+                            <input type="text" class="form-control" name="mobile_number" value="{{ old('mobile_number', $getRecord->mobile_number )}}" placeholder="Phone Number">
                             <div style="color: red">{{$errors->first('mobile_number')}}</div>
                         </div>
 
                         <div class="form-group col-md-6">
                             <label >Admission date <span style="color: red">*</span></label>
-                            <input type="date" class="form-control" name="admission_date" value="{{ old('admission_number' )}}" required >
+                            <input type="date" class="form-control" name="admission_date" value="{{ old('admission_date', $getRecord->admission_date)}}" required >
                             <div style="color: red">{{$errors->first('admission_date')}}</div>
                         </div>
 
@@ -107,25 +107,31 @@
                             <label >Profile Picture <span style="color: red"></span></label>
                             <input type="file" class="form-control" name="profile_pic" value="{{ old('profile_pic' )}}" placeholder="Profile Picture" >
                             <div style="color: red">{{$errors->first('profile_pic')}}</div>
+                            
+                            @if (!empty($getRecord->getProfile()))
+                                
+                                <img src="{{$getRecord->getProfile()}}" alt="" style="width: 100px">
+                                
+                            @endif
                         </div>
 
                         <div class="form-group col-md-6">
                             <label >Blood Group <span style="color: red"></span></label>
-                            <input type="text" class="form-control" name="blood_group" value="{{ old('blood_group' )}}" placeholder="Blood Group" >
+                            <input type="text" class="form-control" name="blood_group" value="{{ old('blood_group', $getRecord->blood_group)}}" placeholder="Blood Group" >
                             <div style="color: red">{{$errors->first('blood_group')}}</div>
                         
                         </div>
 
                         <div class="form-group col-md-6">
                             <label >Height <span style="color: red"></span></label>
-                            <input type="text" class="form-control" name="height" value="{{ old('height' )}}" placeholder="Height" >
+                            <input type="text" class="form-control" name="height" value="{{ old('height', $getRecord->height)}}" placeholder="Height" >
                             <div style="color: red">{{$errors->first('height')}}</div>
                         
                         </div>
 
                         <div class="form-group col-md-6">
                             <label >Weight <span style="color: red"></span></label>
-                            <input type="text" class="form-control" name="weight" value="{{ old('weight' )}}" placeholder="Weight" >
+                            <input type="text" class="form-control" name="weight" value="{{ old('weight', $getRecord->weight)}}" placeholder="Weight" >
                             <div style="color: red">{{$errors->first('weight')}}</div>
                         
                         </div>
@@ -135,9 +141,8 @@
                             <select name="status" id="" class="form-control" required>
 
                                 <option value="">Select Status</option>
-
-                                <option {{(old('status') == 0) ? 'selected' : ''}}  value="0">Active</option>
-                                <option {{(old('status') == 1) ? 'selected' : ''}}  value="1">Inactive</option>
+                                <option {{(old('status', $getRecord->status) == 1) ? 'selected' : ''}}  value="1">Inactive</option>
+                                <option {{(old('status', $getRecord->status) == 0) ? 'selected' : ''}}  value="0">Active</option>
                 
                             </select>
                             <div style="color: red">{{$errors->first('status')}}</div>
@@ -145,13 +150,14 @@
 
                         <div class="form-group col-md-6">
                             <label> Email <span style="color: red">*</span></label>
-                            <input type="email" class="form-control" name="email" required value="{{ old('email') }}" placeholder="Enter email">
+                            <input type="email" class="form-control" name="email" required value="{{ old('email', $getRecord->email) }}" placeholder="Enter email">
                             <div style="color: red">{{$errors->first('email')}}</div>
                         </div>
 
                         <div class="form-group col-md-6">
-                            <label> Password <span style="color: red">*</span></label>
-                            <input type="password" class="form-control" name="password" required value="{{ old('password') }}" placeholder="Password">
+                            <label> Password <span style="color: red"></span></label>
+                            <input type="password" class="form-control" name="password" placeholder="Password">
+                            <p>Do you want to change password so please add new password</p>
                         </div>
 
                     </div>
@@ -162,7 +168,7 @@
                 <div class="card-footer row">
                   <div class="col-md-6"></div>
                   <div class="col-md-2">
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" class="btn btn-primary">Update</button>
                 </div>
                   <div class="col-md-4"></div>
                 </div>
